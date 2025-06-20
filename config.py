178435affedd61cb09942e9c68e8285e0df92fb9
moanbekele <moanbekele@gmail.com>
2025-06-20 10:58:37 +0300

    add coverage

---
+++ config.py
@@ -0,0 +1,18 @@
+class Config:
+    DEBUG = False
+    TESTING = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
+
+class ProductionConfig(Config):
+    pass
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig
+}
